2973b0b338f1c0e0be732c601e4853a678417bd7
abhishektiwari-knoldus <126809890+abhishektiwari-knoldus@users.noreply.github.com>
2023-08-22 17:30:56 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,4 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "backend"
+      storage_account_name = "backendwkej09"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
 
-provider "azurerm" {
-  features {}
 }
