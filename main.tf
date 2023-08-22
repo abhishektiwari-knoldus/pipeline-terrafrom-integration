2ca564d234d9d1c2bb8596332bedd28181eec165
Abhishek <abhishek.tiwari1@knoldus.com>
2023-08-22 12:11:51 +0530

    ok

--- main.tf
+++ main.tf
@@ -1,11 +1,22 @@
+resource "random_string" "rg_suffix" {
+  length  = 6
+  special = false
+}
+resource "random_string" "storage_account_suffix" {
+  length  = 7
+  special = false
+  upper = false
+}
+
+
 
 resource "azurerm_resource_group" "rg" {
-  name     = "Abhishek-rg1"
+  name     = "Abhishek-${random_string.rg_suffix.result}"
   location = "eastus"
 }
 
 resource "azurerm_storage_account" "storageacc" {
-  name                     = "abhishek98765s"
+  name                     = "akashsa${random_string.storage_account_suffix.result}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = var.actTier
@@ -17,7 +28,7 @@ resource "azurerm_storage_account" "storageacc" {
 resource "azurerm_storage_container" "my-Container" {
   name                  = "mycontainer"
   storage_account_name  = azurerm_storage_account.storageacc.name
-  container_access_type = var.conAccessType
+  container_access_type = var.conAccessTypepipeline
 }
 
 resource "azurerm_storage_blob" "file" {
